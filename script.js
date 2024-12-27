3a6846c0a932962a6cb9c84c848bb2fcf1d67a98
Fawadgithub <fa6843891@gmail.com>
2024-12-27 16:43:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+// Task 1: map() Method
+
+// Problem Statement: Ek array [10, 20, 30, 40, 50] diya gaya hai. Is array ke har element ko 5 se multiply karke ek naya array banao.
+
+// const arr = [10, 20, 30, 40, 50];
+// const result = arr.map(num => num * 5);
+// console.log(result);        
+
+//end
+
+// Task 2: filter() Method
+
+// Problem Statement: Ek array [15, 26, 33, 40, 18, 29] diya gaya hai. Sirf even numbers ko filter karke ek naya array banao.
+
+// const arr = [15, 26, 33, 40, 18, 29];
+// const result = arr.filter(num => num % 2 === 0);
+// console.log(result);  
+
+//end
+
+// Task 3: reduce() Method
+
+// Problem Statement: Ek array [5, 10, 15, 20] diya gaya hai. Is array ke sare elements ka sum calculate karo.
+
+// const arr = [5, 10, 15, 20];
+// const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+// console.log(sum);  
+
+//end
+
+
+// Task 4: find() Method
+
+// Problem Statement: Ek array [7, 14, 21, 28, 35] diya gaya hai. Is array me pehla number find karo jo 20 se bada ho.
+
+// const arr = [7, 14, 21, 28, 35];
+// const result = arr.find(num => num > 20);
+// console.log(result);  
+
+//end
+
+// Task 5: findIndex() Method
+
+// Problem Statement: Ek array [3, 5, 7, 9, 11] diya gaya hai. Us element ka index find karo jo 7 ke barabar ho.
+
+// const arr = [3, 5, 7, 9, 11];
+// const index = arr.findIndex(num => num === 7);
+// console.log(index);  
+
+//end
+
+// Task 6: includes() Method
+
+// Problem Statement: Ek array [12, 24, 36, 48, 60] diya gaya hai. Check karo ki 36 is array me hai ya nahi.
+
+    // const arr = [12, 24, 36, 48, 60];
+    // const result = arr.includes(36);
+    // console.log(result); 
+
+//end
+
+// Task 7: indexOf() Method
+
+// Problem Statement: Ek array [1, 2, 3, 4, 2, 5] diya gaya hai. Array me 2 ka index find karo
+
+// const arr = [1, 2, 3, 4, 2, 5];
+// const index = arr.indexOf(2);
+// console.log(index);  
+
+
+//end
+
+// Task 8: lastIndexOf() Method
+
+// Problem Statement: Ek array [1, 2, 3, 4, 2, 5] diya gaya hai. Array me 2 ka last index find karo.
+
+const arr = [1, 2, 3, 4, 2, 5];
+const lastIndex = arr.lastIndexOf(2);
+console.log(lastIndex);  
+
+//end
+
+
